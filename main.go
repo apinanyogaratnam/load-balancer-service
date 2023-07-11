b47f5a4150f65beefa63a0b019494351bf2ea377
apinanyogaratnam <apinan@ontopical.com>
2023-07-11 07:45:20 -0400

    refactor: scaling urls available to add to the load balancer

--- main.go
+++ main.go
@@ -26,38 +26,29 @@ func getMinLoad(load1 *Load, load2 *Load) *Load {
 }
 
 func main() {
-	resp, err := http.Get("http://localhost:8001")
-	if err != nil {
-		log.Fatalln("Error calling server 1")
+	urls := []string{
+		"http://localhost:8001",
+		"http://localhost:8002",
+		"http://localhost:8003",
 	}
-	defer resp.Body.Close()
-	load := new(Load)
-	load.ServerId = 1
-	json.NewDecoder(resp.Body).Decode(&load)
-	log.Println(load)
-	minLoad := load
 
-	resp1, err := http.Get("http://localhost:8002")
-	if err != nil {
-		log.Fatalln("Error calling server 2")
+	minLoad := new(Load)
+	for i, url := range urls {
+		resp, err := http.Get(url)
+		if err != nil {
+			log.Fatalln("Error calling server:", i+1, err)
+		}
+		defer resp.Body.Close()
+		load := new(Load)
+		load.ServerId = i + 1
+		json.NewDecoder(resp.Body).Decode(&load)
+		log.Println(load)
+
+		if i == 0 {
+			minLoad = load
+		} else {
+			minLoad = getMinLoad(minLoad, load)
+		}
 	}
-	defer resp1.Body.Close()
-	load1 := new(Load)
-	load1.ServerId = 2
-	json.NewDecoder(resp1.Body).Decode(&load1)
-	log.Println(load1)
-	minLoad = getMinLoad(minLoad, load1)
-
-	resp2, err := http.Get("http://localhost:8003")
-	if err != nil {
-		log.Fatalln("Error calling server 3")
-	}
-	defer resp2.Body.Close()
-	load2 := new(Load)
-	load2.ServerId = 3
-	json.NewDecoder(resp2.Body).Decode(&load2)
-	log.Println(load2)
-	minLoad = getMinLoad(minLoad, load2)
-
-	log.Println("min load", minLoad)
+	log.Println("Min load is:", minLoad)
 }
