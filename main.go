67f90136e22fa98389f6107501265d09b9444d9c
apinanyogaratnam <apinan@ontopical.com>
2023-07-10 23:20:54 -0400

    feat: finding minimum load server

--- main.go
+++ main.go
@@ -10,8 +10,20 @@ type Load struct {
 	Cpu     float64 `json:"cpu"`
 	Memory  float64 `json:"memory"`
 	Disk    float64 `json:"disk"`
+	ServerId int    `json:"server_id"`
 }
 
+func (load Load) sum() float64 {
+	return load.Cpu + load.Memory + load.Disk
+}
+
+func getMinLoad(load1 *Load, load2 *Load) *Load {
+	if load1.sum() < load2.sum() {
+		return load1
+	}
+
+	return load2
+}
 
 func main() {
 	resp, err := http.Get("http://localhost:8001")
@@ -20,8 +32,10 @@ func main() {
 	}
 	defer resp.Body.Close()
 	load := new(Load)
+	load.ServerId = 1
 	json.NewDecoder(resp.Body).Decode(&load)
 	log.Println(load)
+	minLoad := load
 
 	resp1, err := http.Get("http://localhost:8002")
 	if err != nil {
@@ -29,8 +43,10 @@ func main() {
 	}
 	defer resp1.Body.Close()
 	load1 := new(Load)
+	load1.ServerId = 2
 	json.NewDecoder(resp1.Body).Decode(&load1)
 	log.Println(load1)
+	minLoad = getMinLoad(minLoad, load1)
 
 	resp2, err := http.Get("http://localhost:8003")
 	if err != nil {
@@ -38,6 +54,10 @@ func main() {
 	}
 	defer resp2.Body.Close()
 	load2 := new(Load)
+	load2.ServerId = 3
 	json.NewDecoder(resp2.Body).Decode(&load2)
 	log.Println(load2)
+	minLoad = getMinLoad(minLoad, load2)
+
+	log.Println("min load", minLoad)
 }
